1908a2d10739c7d7f4909610f88f420570c7fad5
Sunny4123 <151112974+Sunny4123@users.noreply.github.com>
2024-08-17 15:47:25 +0700

    Lab9

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import data from '../data';
 import Product from './product';
 import AddForm from './product/AddFrom';
 
-let id = 9;
+let currentProductId = 9;
 
 export default function Home() {
   const [products, setProducts] = useState([]);
- 
+
+  useEffect(() => {
+    async function getProducts() {
+      const products = await axios.get(
+        'https://apimocha.com/prosunny555/product'
+      );
+      setProducts(products.data);
+    }
+  
+    getProducts();
+  }, []); // Put the empty array to make sure that the hook is executed only once
 
   function addProduct(product) {
-    const newProduct = { id: ++id, ...product };
+    const newProduct = { id: ++currentProductId, ...product };
     setProducts([...products, newProduct]);
   }
 
   return (
     <>
       <h1>New Products</h1>
-      {
-        products.length > 0 ? (
-          <ul className="Home__products">
-            {products.map((product) => (
-              <Product key={product.id} item={product} />
-            ))}
-          </ul>
-        ) : (
-          <div>Loading products....</div>
-        )
-      }
+      {products.length > 0 ? (
+        <ul className="Home__products">
+          {products.map((product) => (
+            <Product key={product.id} item={product} />
+          ))}
+        </ul>
+      ) : (
+        <div>Loading products....</div>
+      )}
       <AddForm addProduct={addProduct} />
     </>
   );
